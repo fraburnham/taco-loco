b4a1cb4552752862c27ee90344ea9196025f80e3
Frank Burnham <frank@n8git.dev>
2020-07-09 10:25:17 -0400

    Initial

---
+++ src/main/java/dev/n8git/detroitlabstest/DetroitLabsTestApplication.java
@@ -0,0 +1,13 @@
+package dev.n8git.detroitlabstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetroitLabsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DetroitLabsTestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/dev/n8git/detroitlabstest/ServletInitializer.java
@@ -0,0 +1,13 @@
+package dev.n8git.detroitlabstest;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DetroitLabsTestApplication.class);
+    }
+
+}
