892e00dfb4822997fa92512b23bc3a7817a886a7
Frank Burnham <frank@n8git.dev>
2020-07-09 10:22:06 -0400

    `Total` Describes api output format

---
+++ src/main/java/dev/n8git/detroitlabstest/Total.java
@@ -0,0 +1,14 @@
+package dev.n8git.detroitlabstest;
+
+public class Total {
+
+    private final int totalCents;
+
+    public Total(int totalCents) {
+        this.totalCents = totalCents;
+    }
+
+    public int getTotalCents() {
+        return totalCents;
+    }
+}
